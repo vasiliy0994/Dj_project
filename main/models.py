c5dac2d7d765dc634566b2a468801adfd2a4a55e
vasiliy0994 <vasiliy_kulikov_1994@mail.ru>
2024-03-30 13:23:25 +0700

    Initial commit

---
+++ main/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class News(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True)
+    created_ad = models.DateTimeField(auto_now_add=True)
+    updated_ad = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='medin/%Y/%m/%d')
+    is_published = models.BooleanField(default=True)
